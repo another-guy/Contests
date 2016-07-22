0175a21dc107e2c432a67ad3f924d2f844d7534b
Igor Soloydenko <soloydenko@gmail.com>
2016-07-22 16:10:16 -0700

    Add empty template.py

---
+++ _common_py/template.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    # Read input
+    
+    # Do work
+    
+    # Write result to output  
+    sys.stdout.write(result + '\n')
+
+if __name__ == '__main__':
+    main()
