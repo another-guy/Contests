b48e95dbe17001a599e8a207d538dc02a63254ba
Igor Soloydenko <soloydenko@gmail.com>
2017-06-15 00:07:25 -0700

    NPM package with webpack

---
+++ ctci/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './index.ts',
+    output: {
+        path: __dirname + '/dist',  
+        filename: 'bundle.js',
+        publicPath: './dist/'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+}
