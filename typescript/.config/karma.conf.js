b82a26ac95afc3f90c95241ce11a18d1868d3a78
Igor Soloydenko <soloydenko@gmail.com>
2017-10-26 01:55:15 -0700

    Fix karma-typescript Cannot find name 'Set' error

--- typescript/.config/karma.conf.js
+++ typescript/.config/karma.conf.js
@@ -36,6 +36,10 @@ module.exports = function(config) {
       ],
     },
 
+    karmaTypescriptConfig: {
+      tsconfig: "./tsconfig.json",
+    },
+
     // test results reporter to use. possible values: 'dots', 'progress'. available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: [
       'progress',
